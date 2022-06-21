3077dafafb056656b957a74ed4c50e537faac874
Vaidas Makstutis <vaidas.makstutis@gmail.com>
2022-06-21 14:22:42 +0300

    Page loader

--- src/containers/Countries/CountriesContainer.tsx
+++ src/containers/Countries/CountriesContainer.tsx
@@ -1,10 +1,23 @@
 import { useQuery } from "react-query";
+import { Container } from "@mui/material";
 import { getCountries } from "../../api/countries";
 import CountryCard from "../../Components/CountryCard";
+import PageLoader from "../../Components/Loader/PageLoader";
 
 const CountriesContainer = () => {
   const { data, isLoading } = useQuery("countries", getCountries);
-  return <CountryCard />;
+
+  if (isLoading) {
+    return <PageLoader />
+  }
+
+  return (
+    <Container maxWidth="md">
+      <Container disableGutters sx={{ display: "grid", gridTemplateColumns: "1fr 1fr 1fr", gridGap: "20px" }}>
+        {data?.map(country => <CountryCard key={country.country} {...country} />) || <div>No Countries</div>}
+      </Container>
+    </Container>
+  );
 };
 
 export default CountriesContainer;
