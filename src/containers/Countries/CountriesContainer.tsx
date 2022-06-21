4c90666ce9c0a5491bb6c1c3a008f5df62832067
Vaidas Makstutis <vaidas.makstutis@gmail.com>
2022-06-21 17:35:42 +0300

    Search country

--- src/containers/Countries/CountriesContainer.tsx
+++ src/containers/Countries/CountriesContainer.tsx
@@ -1,20 +1,54 @@
+import React, { useState, useEffect } from "react";
 import { useQuery } from "react-query";
-import { Container } from "@mui/material";
-import { getCountries } from "../../api/countries";
+import { Container, TextField } from "@mui/material";
+import { getCountries, Country } from "../../api";
 import CountryCard from "../../Components/CountryCard";
 import PageLoader from "../../Components/Loader/PageLoader";
 
 const CountriesContainer = () => {
   const { data, isLoading } = useQuery("countries", getCountries);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [searchResult, setSearchResult] = useState<Country[]>([]);
+
+  useEffect(() => {
+    if (data?.length) {
+      setSearchResult(data);
+    }
+  }, [data])
 
   if (isLoading) {
     return <PageLoader />
   }
+  
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (data?.length) {
+      const query = e.target.value.toLowerCase();
+      const result = data.filter((country) => {
+        return country.country.toLowerCase().includes(query);
+      })
+      setSearchResult(result);
+      setSearchQuery(query);
+    }
+  }
 
   return (
     <Container maxWidth="md">
-      <Container disableGutters sx={{ display: "grid", gridTemplateColumns: "1fr 1fr 1fr", gridGap: "20px" }}>
-        {data?.map(country => <CountryCard key={country.country} {...country} />) || <div>No Countries</div>}
+      <TextField 
+      label="Search" 
+      sx={{margin: "20px 0"}} 
+      fullWidth 
+      placeholder="Search country"
+      value={searchQuery}
+      onChange={handleSearch}
+      />
+      <Container disableGutters 
+      sx={{ 
+        display: "grid", 
+        gridTemplateColumns: "1fr 1fr 1fr", 
+        gridGap: "20px" 
+        }}
+        >
+        {searchResult?.map(country => <CountryCard key={country.country} {...country} />) || <div>No Countries</div>}
       </Container>
     </Container>
   );
